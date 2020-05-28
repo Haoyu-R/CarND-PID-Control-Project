4b8315ad8f254539814d9f2dab37f81acb83cd40
Haoyu <arhyrhy185@gmail.com>
2020-05-28 15:00:11 +0200

    Finish project

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
@@ -9,22 +10,102 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+    /**
+     * TODO: Initialize PID coefficients (and errors, if needed)
+     */
+    K[0] = Kp_;
+    K[1] = Ki_;
+    K[2] = Kd_;
 }
 
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+// void PID::Init_d(double dp, double di, double dd){
+//   d[0] = dp;
+//   d[1] = di;
+//   d[2] = dd; 
+// }
 
+void PID::UpdateError(double cte) {
+    /**
+     * TODO: Update PID errors based on cte.
+     */
+     // if(twiddle_mode){
+     //   twiddle(cte);
+     // }
+    p_error = -K[0] * cte;
+    i_error -= K[1] * cte;
+
+    d_error = -K[2] * (cte - last_d_error);
+    last_d_error = cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    /**
+     * TODO: Calculate and return the total error
+     */
+    return p_error + i_error + d_error;  // TODO: Add your total error calc here!
 }
+
+// bool PID::tol_reached(double tol){
+
+//      if(d[0]+d[1]+d[2] < tol){
+//         return true;
+//      }
+//      return false;
+// }
+
+// void PID::twiddle(double cte){
+
+//   if(tol_reached(0.00001)){
+//     std::cout << "Best Kp: " << K[0] << " Best Ki: " << K[1] << " Best Kd:" << K[2]
+//           << std::endl;
+//     while(true){};
+//   }
+
+//   // if(counter == 0){
+//   //   K[index] += d[index];
+//   //   counter++;
+//   // }
+
+//   counter++;
+
+//   if(counter > 100){
+//     err += cte;
+//   }
+
+//   if(counter > 1100){
+//     counter = 0;
+
+//     switch (case_switch)
+//     {
+//     case 0:
+//       if(err < best_err){
+//         best_err = err;
+//         d[index] *= 1.1;
+//         index = (index+1) %2;
+//         K[index] += d[index];
+//       }else{
+//         case_switch = 1;
+//         K[index] -= 2*d[index];
+//       }
+//       break;
+//     case 1:
+//       if(err < best_err){
+//         best_err = err;
+//         d[index] *= 1.1;
+//         index = (index+1) %2;
+//         K[index] += d[index];
+//         case_switch = 0;
+//       }else{
+//         K[index] += d[index];
+//         d[index] *= 0.9;
+//         index = (index+1) %2;
+//         K[index] += d[index];
+//         case_switch = 0;
+//       }
+//       break;
+//     }
+
+//     err = 0;
+//   }
+
+// }
--- src/PID.h
+++ src/PID.h
@@ -1,50 +1,70 @@
 #ifndef PID_H
 #define PID_H
+#include <limits>
+#include <vector>
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
-  PID();
+public:
+    /**
+     * Constructor
+     */
+    PID();
 
-  /**
-   * Destructor.
-   */
-  virtual ~PID();
+    /**
+     * Destructor.
+     */
+    virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_);
+    /**
+     * Initialize PID.
+     * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+     */
+    void Init(double Kp_, double Ki_, double Kd_);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
-  void UpdateError(double cte);
+    /**
+     * Update the PID error variables given cross track error.
+     * @param cte The current cross track error
+     */
+    void UpdateError(double cte);
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
-  double TotalError();
+    //   void Init_d(double dp, double di, double dd);
 
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+    //   bool tol_reached(double tol);
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    //   void twiddle(double cte);
+
+      /**
+       * Calculate the total PID error.
+       * @output The total PID error
+       */
+    double TotalError();
+
+
+    //   bool twiddle_mode = false;
+
+private:
+    /**
+     * PID Errors
+     */
+    double p_error;
+    double i_error;
+    double d_error;
+    double last_d_error = 0;
+
+    //   std::vector<double> d{1, 1, 1};
+
+    //   double best_err = std::numeric_limits<double>::max();
+    //   double err = 0;
+
+    //   int counter = 0;
+    //   int index = 0;
+
+    //   int case_switch = 0;
+    //   /**
+    //    * PID Coefficients
+    //    */ 
+    std::vector<double> K{ 0, 0, 0 };
+
 };
 
 #endif  // PID_H
